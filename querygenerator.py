2c2c74e5545a57373872f212b0b4f78f3878ec20
syigit <syigitisk@gmail.com>
2012-01-20 14:16:09 +0000

    * logger.py /tmp/nfquery.log ' a da yazmaya basladi,    parserlarin apayri bir dosyada olsalar bile logger ' i    kullanip ayni anda ayni dosyaya loglayabilecegi test edildi.
     * Bu adimdan sonra, parserlar artik kendi baslarina calisacaklar,
       yani querygenerator tarafindan import edilmeleri ortadan kalkacak.
       Her parser kendi outputunu uretecek, querygenerator bu outputu isleyip
       db ' ye atacak. 
       Parserlar qgenerator tarafindan da calistirilabilirler, ya da crontab ' a 
       eklenebilirler. Bu sistemin kurulumunu zorlastirabilir, ama denemek lazim. 
       Bu durumda bir api saglamayacagiz ve parserlarin fonksiyonlarini da import
       etmeyecegiz.
    
     * Yinede bunlar kesin degil ;)

--- querygenerator.py
+++ querygenerator.py
@@ -9,7 +9,7 @@ import hashlib
 import MySQLdb
 
 from apscheduler.scheduler import Scheduler
-
+from apscheduler.scheduler import logger as schlogger
 
 # nfquery imports
 from query import query
@@ -64,11 +64,17 @@ class QueryGenerator(multiprocessing.Process):
     
     def __init__(self, sources):
         self.sources = sources
+        # start logging
         logging.setLoggerClass(ColoredLogger)
         self.qglogger = logging.getLogger('QueryGenerator')
         self.qglogger.setLevel(defaults.loglevel)
+        # get db connection and the cursor
         self.connection = db.get_database_connection()
         self.cursor = self.connection.cursor()
+        # Start the scheduler
+        self.sched = Scheduler()
+        self.sched.start()
+        
 
     def run(self):
         # Check for reconfiguration
@@ -83,16 +89,9 @@ class QueryGenerator(multiprocessing.Process):
             self.executeParsers()
             self.subscription = subscription()
             self.createSubscriptions()
-            self.schedule()
     
-    def schedule(self):        
-        # Start the scheduler
-        sched = Scheduler()
-        sched.start()
-        
+#    def schedule(self, parser, time_interval): 
         ## Schedule job_function to be called every two hours
-        sched.add_interval_job(self.executeParsers, minutes=1, start_date='2012-01-18 09:30')
-        print  'scheduled'
 
 
     def reconfigureSources(self):
@@ -202,29 +201,47 @@ class QueryGenerator(multiprocessing.Process):
                 self.qglogger.warning('Parser %s doesn\'t exist\nPlease check the nfquery.conf file' % self.sources[i].parser)
 
     
-    def executeParsers(self):
+    def executeParsers(self, parser=None):
+        sys.path.append(defaults.sources_path)
         self.qglogger.debug('In %s' % sys._getframe().f_code.co_name)
-        for i in range(len(self.sources)):
-            # import parsers
-            sys.path.append(defaults.sources_path)
-            try:
-                # This function is invoked by the import statement. It can be replaced (by importing the __builtin__ module and assigning to __builtin__.__import__) in order to change semantics of 
-                # the import statement, but nowadays it is usually simpler to use import hooks (see PEP 302). 
-                # !!! Direct use of __import__() is rare, except in cases WHERE YOU WANT TO IMPORT A MODULE WHOSE NAME IS ONLY KNOWN AT RUNTIME. 
-                
-                parser = __import__(('parsers.' + self.sources[i].parser.split('/').pop()).split('.py')[0], globals(), locals(), ['fetch_source','parse_source'], -1)
-                #'from ' + (self.sources[i].parser.split('/').pop()).split('.py')[0] + ' import fetch_source, parse_source'
-                 
-                # call generic parser modules
-                parser.fetch_source(self.sources[i].sourcelink, self.sources[i].sourcefile)
-                result = parser.parse_source(self.sources[i].sourcename, self.sources[i].sourcefile)
-                print result
-                if result > 0:
-                    self.qglogger.error('%s is exited with error code %d!' % (parser, result))
-            except Exception, e:
-                self.qglogger.error('got exception: %r, exiting' % (e))
-                sys.exit()
-            
+        if parser is None:
+            self.qglogger.info('running all parsers')
+            for i in range(len(self.sources)):
+                # import parsers
+                try:
+                    #parser = __import__(('parsers.' + self.sources[i].parser.split('/').pop()).split('.py')[0], globals(), locals(), ['fetch_source','parse_source'], -1)
+                    exec('from ' + (self.sources[i].parser.split('/').pop()).split('.py')[0] + ' import fetch_source, parse_source')
+                    # call generic parser modules
+                    #parser.fetch_source(self.sources[i].sourcelink, self.sources[i].sourcefile)
+                    #fetch_source(self.sources[i].sourcelink, self.sources[i].sourcefile)
+                    #result = parser.parse_source(self.sources[i].sourcename, self.sources[i].sourcefile)
+                    #result = parse_source(self.sources[i].sourcename, self.sources[i].sourcefile)
+                    parse_source(self.sources[i].sourcename, self.sources[i].sourcefile)
+                    #if result > 0:
+                    #    #self.qglogger.error('%s is exited with error code %d!' % (parser, result))
+                    #    self.qglogger.error('%s is exited with error code %d!' % (self.sources[i].parser, result))
+                    # Schedule the job for interval running
+                    #self.sched.add_interval_job(fetch_source, minutes=self.sources[i].time_interval, start_date='2012-01-18 09:30')
+                    self.sched.add_interval_job(fetch_source, args=[self.sources[i].sourcelink, self.sources[i].sourcefile], minutes=1)
+                    print fetch_source
+                    self.sched.add_interval_job(parse_source, args=[self.sources[i].sourcename, self.sources[i].sourcefile], minutes=1)
+                    print parse_source
+                except Exception, e:
+                    self.qglogger.error('got exception: %r, exiting' % (e))
+                    sys.exit()
+        else:
+            self.qglogger.info('running only parser %s' % parser)
+            for i in range(len(self.sources)):
+                if self.sources[i].parser == parser:
+                    try:
+                        exec('from ' + (self.sources[i].parser.split('/').pop()).split('.py')[0] + ' import fetch_source, parse_source')
+                        #fetch_source(self.sources[i].sourcelink, self.sources[i].sourcefile)
+                        parse_source(self.sources[i].sourcename, self.sources[i].sourcefile)
+                    except Exception, e:
+                        self.qglogger.error('got exception: %r, exiting' % (e))
+                        sys.exit()
+        #self.sched.print_jobs()
+           
 
     def createSubscriptions(self):
         self.qglogger.debug('In %s' % sys._getframe().f_code.co_name)
