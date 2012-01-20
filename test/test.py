607a2ba80ee6a6b267d0978d8ecb297f04986ef0
syigit <syigitisk@gmail.com>
2012-01-20 14:16:09 +0000

    * logger.py /tmp/nfquery.log ' a da yazmaya basladi,    parserlarin apayri bir dosyada olsalar bile logger ' i    kullanip ayni anda ayni dosyaya loglayabilecegi test edildi.
     * Bu adimdan sonra, parserlar artik kendi baslarina calisacaklar,
       yani querygenerator tarafindan import edilmeleri ortadan kalkacak.
       Her parser kendi outputunu uretecek, querygenerator bu outputu isleyip
       db ' ye atacak. 
       Parserlar qgenerator tarafindan da calistirilabilirler, ya da crontab ' a 
       eklenebilirler. Bu sistemin kurulumunu zorlastirabilir, ama denemek lazim. 
       Bu durumda bir api saglamayacagiz ve parserlarin fonksiyonlarini da import
       etmeyecegiz.
    
     * Yinede bunlar kesin degil ;)

--- test/test.py
+++ test/test.py
@@ -1,15 +1,23 @@
 #!/usr/local/bin/python
 
-import time
-from db import SingletonDatabase
+#import time
+#from db import SingletonDatabase
+#
+#def printDB():
+#    test1 = SingletonDatabase.getDatabaseConnection()
+#    test2 = SingletonDatabase.getDatabaseConnection()
+#    test3 = SingletonDatabase.getDatabaseConnection()
+#    test4 = SingletonDatabase.getDatabaseConnection()
+#    print "id of first connection = %s\n" % id(test1)
+#    print "id of second connection = %s\n" % id(test2)
+#    print "id of third connection = %s\n" % id(test3)
+#    print "id of fourth connection = %s\n" % id(test4)
+#    time.sleep(30)
 
-def printDB():
-    test1 = SingletonDatabase.getDatabaseConnection()
-    test2 = SingletonDatabase.getDatabaseConnection()
-    test3 = SingletonDatabase.getDatabaseConnection()
-    test4 = SingletonDatabase.getDatabaseConnection()
-    print "id of first connection = %s\n" % id(test1)
-    print "id of second connection = %s\n" % id(test2)
-    print "id of third connection = %s\n" % id(test3)
-    print "id of fourth connection = %s\n" % id(test4)
-    time.sleep(30)
+from logger import ColoredLogger
+import logging
+# Start Logging Module
+logging.setLoggerClass(ColoredLogger)
+nfquerylog = logging.getLogger('nfquery')
+
+logging.info('aaa')
