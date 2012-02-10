0b1cbafcfde8919544200e8c240cdc6787436fbc
syigit <syigitisk@gmail.com>
2012-02-10 08:02:42 +0000

    * Daily commit.

--- nfquery/defaults.py
+++ nfquery/defaults.py
@@ -3,6 +3,6 @@
 class defaults:
     path = '/usr/local/nfquery'
     sources_path = path + '/sources'
-    reconfigure_flag = 0
+    reconfig_flag = 0
     loglevel = 0
     conf_file = '/etc/nfquery.conf'
