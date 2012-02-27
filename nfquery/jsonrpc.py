f4fbe47b932b8e71bbcd8aeb4de0bbc5d3f57dbd
syigit <syigitisk@gmail.com>
2012-02-27 08:31:55 +0000

    * Registration method is completed not the function itself totaly,    rpcserver class renamed and db connection edited again.

--- nfquery/jsonrpc.py
+++ nfquery/jsonrpc.py
@@ -1,16 +1,19 @@
 #!/usr/local/bin/python
 
 
+import sys
+import hashlib
+import time
+
 from txjsonrpc.web import jsonrpc
 from twisted.web import server
 from twisted.internet import reactor, ssl
 from twisted.application import service,internet
-import sys
 
 import db
 from models import Plugin 
 
-class Example(jsonrpc.JSONRPC):
+class RPCServer(jsonrpc.JSONRPC):
     """
     An example object to be published.
     """
@@ -37,19 +40,40 @@ class Example(jsonrpc.JSONRPC):
         raise jsonrpc.Fault(123, "The fault procedure is faulty.")
 
 
-    def jsonrpc_register(self, organization, adm_name, adm_mail, adm_tel, adm_publickey, prefix_list, plugin_ip):
-        print "Registration information : %s,%s,%s,%s,%s,%s,%s" % (organization, adm_name, adm_mail, adm_tel, adm_publickey, prefix_list, plugin_ip)
-        store = db.get_store()
-        plugin_list = store.find(Plugin)
-        name_list = []
-        for plugin in plugin_list:
-            print name_list.append(plugin.organization)
-        return name_list
+    def jsonrpc_register(self, organization, adm_name, adm_mail, adm_tel, adm_publickey_file, prefix_list, plugin_ip):
+        # DEBUG mode da hangi fieldlarin hatali geldigini yazdirabiliriz tabiki sadece query server ' a.
+        #print "Registration information : %s,%s,%s,%s,%s,%s,%s" % (organization, adm_name, adm_mail, adm_tel, adm_publickey, prefix_list, plugin_ip)
+        self.store = db.get_store()
+        plugin = self.store.find(Plugin, Plugin.organization == unicode(organization)).one()
+        if plugin is None:
+            message =  'Your plugin is not registered to QueryServer yet.'
+            message += 'Plugin is not found.'
+            message += 'Please ask to QS Administrator about your registration process.'
+            print message
+            return message
+        else:
+            checksum = hashlib.md5()
+            checksum.update( organization + adm_name + adm_mail + adm_tel +
+                             adm_publickey_file + prefix_list + plugin_ip  )
+            if checksum.hexdigest() != plugin.checksum:
+                message = 'Your plugin information doesn\'t match with the QueryServer side.'
+                message += 'Plugin Checksum Error'
+                message += 'Please check your information and try again.'
+                print message
+                return message 
+            elif checksum.hexdigest() == plugin.checksum:
+                # Set the plugin registered
+                plugin.registered = True
+                self.store.add(plugin)
+                self.store.commit()
+                message =  'Your plugin is registered.'
+                message += 'Feel free to checkout query subscriptions.'
+                print message
+                time.sleep(30)
+                #return self.sendSubscriptionTypes()
+       
 
-        
-        
-
-
+ 
 #def getExampleService():
 #    r = Example()
 #    exserver = server.Site(r)
@@ -58,15 +82,18 @@ class Example(jsonrpc.JSONRPC):
 
 # ------------------------------------------------- # ------------------------------------------------ # ---------------------# 
 
-if __name__ == '__main__':
-    print dir(ssl.SSL)
-    sys.exit()
-    r = Example()
-    exserver = server.Site(r) 
-    #reactor.listenSSL(7777, exserver, ssl.DefaultOpenSSLContextFactory('certs/nfquery.key', 'certs/nfquery.crt'))
-    reactor.listenSSL(7777, exserver, ssl.DefaultOpenSSLContextFactory('/home/serdar/workspace/nfquery/cfg/certs/nfquery.key','/home/serdar/workspace/nfquery/cfg/certs/nfquery.crt'))
-    reactor.run()
-    print 'main'
+#if __name__ == '__main__':
+#    print dir(ssl.SSL)
+#    sys.exit()
+#    r = Example()
+#    exserver = server.Site(r) 
+#    #reactor.listenSSL(7777, exserver, ssl.DefaultOpenSSLContextFactory('certs/nfquery.key', 'certs/nfquery.crt'))
+#    reactor.listenSSL(7777, exserver, ssl.DefaultOpenSSLContextFactory('/home/serdar/workspace/nfquery/cfg/certs/nfquery.key','/home/serdar/workspace/nfquery/cfg/certs/nfquery.crt'))
+#    reactor.run()
+#    print 'main'
+
+
+
 #else:
 #    application=service.Application('Example Application')
 #    #service = reactor.listenSSL(7777, server.Site(r),ssl.DefaultOpenSSLContextFactory('certs/nfquery.key', 'certs/nfquery.crt'))
