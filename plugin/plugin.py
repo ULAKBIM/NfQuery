329d18490ffec79302e081f979bc4e548dc0ccff
syigit <syigitisk@gmail.com>
2012-03-07 07:49:38 +0000

    * P{lugin and QS integration process continuing

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -12,16 +12,17 @@ import logging
 class Plugin:
 
     def __init__(self):
-        logger.LOGLEVEL = logging.DEBUG
+        #logger.LOGLEVEL = logging.DEBUG
+        logger.LOGLEVEL = logging.INFO
         self.plogger = logger.createLogger('Plugin')
         self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         self.proxy = Proxy('https://127.0.0.1:7777/')
-        self.subscription_list = None
+        #self.subscription = None
+        self.chosen = None
         self.reactor = reactor
-        self.deferredlist = []
 
     def parseConfig(self, conf_file):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         # Parse Config File
         try:
             self.config = Config(conf_file)
@@ -32,38 +33,40 @@ class Plugin:
 
 
     def printError(self, error):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
-        print 'error', error
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
+        #print 'error', error
+        self.shutDown()
 
-    
-    def shutDown(self, data):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+
+    def shutDown(self):
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         self.plogger.info("Shutting down reactor...")
         self.reactor.stop()
-   
 
+   
     def getSubscriptionInformation(self, subscription):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         self.plogger.info('getting subscription information %s' % subscription)
         return self.proxy.callRemote('get_subscription', subscription)
 
 
     def printSubscriptionDetails(self, subscription):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         if subscription is None:
             self.plogger.warning('Subscription info couldn\'t be gathered. Please try another subscription option : ')
         else:
             self.plogger.info('Here is the subscription information : ')
             print subscription
-        self.start()
+        #self.start()
+        #self.shutDown()
         #self.printValue(name)
 
 
     def chooseSubscription(self, value):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         if value is None:
-            print 'we don\'t have any subscriptions yet in QueryServer ;(\n'
-            raise Exception, "No Query for subscription error";
+            self.plogger.warning('we don\'t have any subscriptions yet in QueryServer ;(\n')
+            raise Exception, "No Query for subscription error"
         else:
             for index in range(len(value)):
                 self.plogger.info("%d  : %s " % (index, value[index]))
@@ -72,23 +75,23 @@ class Plugin:
                 self.plogger.info("Please enter your subscription number : ")
                 index = raw_input()
                 if index == 'q':
-                    self.plogger.warning("Don want to choose a subscription, quitting.")
-                    self.shutDown()
-                try:
-                    index = int(index)
-                    if index < len(value):
-                        return value[int(index)]
-                    else:
+                    self.plogger.warning("Quitting.")
+                    raise Exception, 'Quitting.'
+                else:
+                    try:
+                        index = int(index)
+                        if index < len(value):
+                            return value[int(index)]
+                        else:
+                            self.plogger.error('Please enter the number in valid range or "q" for quitting.')
+                    except Exception, e:
+                        self.plogger.error('%s' % e)
                         self.plogger.error('Please enter the number in valid range or "q" for quitting.')
-                except Exception, e:
-                    self.plogger.error('%s' % e)
-                    self.plogger.error('Please enter the number in valid range or "q" for quitting.')
 
 
-    def getSubscriptionList(self, value=None):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+    def getSubscriptionList(self):
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         return self.proxy.callRemote('get_subscriptions')
-
 
     #def register(self):
     #    self.plogger.info('In %s' % sys._getframe().f_code.co_name)
@@ -100,7 +103,7 @@ class Plugin:
 
 
     def start(self):
-        self.plogger.info('In %s' % sys._getframe().f_code.co_name)
+        self.plogger.debug('In %s' % sys._getframe().f_code.co_name)
         
         #####   run it reverse way ###  d1 = self.getSubscriptionList()
         #####   run it reverse way ###  #d1.addCallbacks(self.chooseSubscription, self.printError)
@@ -115,18 +118,32 @@ class Plugin:
         #####   run it reverse way ###  #d4.addCallbacks(self.getSubscriptionList, self.printError)
         #####   run it reverse way ###  d4.addCallbacks(d1, self.printError)
 
+
         d1 = self.getSubscriptionList()
+        d1.addCallbacks(self.chooseSubscription)
+        d1.addCallbacks(self.getSubscriptionInformation)
+        d1.addCallbacks(self.printSubscriptionDetails)
+        d1.addErrback(self.printError)
+
+        self.reactor.callLater(1, self.start)
+
         #d2 = self.chooseSubscription
         #d3 = self.getSubscription()
         #d4 = self.printSubscriptionDetails
-        d1.addCallbacks(self.chooseSubscription, self.printError)
-        d1.addCallback(self.getSubscriptionInformation)
-        d1.addCallback(self.printSubscriptionDetails)
+                #d1.addErrback(self.printError)
         #d3.addCallback(d1)
         #d1.addCallback(d1)
         #d2.addCallbacks(d3, self.printError)
         #d3.addCallbacks(d4, self.printError)
         #d4.addCallbacks(d1, self.printError)
+ 
+    def run(self):
+        try:
+            self.reactor.run()
+        except KeyboardInterrupt:
+            print "You hit control-c"
+            self.shutDown()
+
 
 
 if __name__ == "__main__":
@@ -134,10 +151,6 @@ if __name__ == "__main__":
     p = Plugin()
     p.parseConfig('./plugin.conf')
     p.start()
-    try:
-        p.reactor.run()
-    except KeyboardInterrupt:
-        print "You hit control-c"
-        p.shutDown()
-
+    p.run()
+    
 
