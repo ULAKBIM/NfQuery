4ff51591c0eee3ebd8cc332c56eee56559ba68b9
syigit <syigitisk@gmail.com>
2012-03-30 11:08:43 +0000

    * Query creation mechanism is changed after the discussion with Murat and Emre, 	so database design is changed some.

--- nfquery/models.py
+++ nfquery/models.py
@@ -1,172 +1,6 @@
 from storm.locals import *
 
 
-class Domain(object):
-    
-    __storm_table__ = 'domain'
-
-    id = Int(primary=True)
-    name = Unicode()
-
-
-class IP(object):
-
-    __storm_table__ = 'ip'
-
-    id = Int(primary=True)
-    ip = Unicode()
-    ip_int = Int()
-
-
-class IPPort(object):
-    
-    __storm_table__ = 'ip_port'
-
-    id = Int(primary=True)
-    ip_port = Unicode()
-    format_ = Int()
-
-
-class Port(object):
-
-    __storm_table__ = 'port'
-
-    id = Int(primary=True)
-    port = Int()
-
-
-class Parser(object):
-   
-    __storm_table__ = 'parser'
-
-    id = Int(primary=True)
-    name = Unicode()
-    time_interval = Int()
-
-
-class List(object):
-
-    __storm_table__ = 'list'
-    
-    id  = Int(primary=True)
-    type = Unicode()
-
-
-class Source(object):
-
-    __storm_table__ = 'source'
-
-    id = Int(primary=True)
-    name = Unicode()
-    link = Unicode()
-    checksum = Unicode()
-    list_id = Int()
-    parser_id = Int()
-
-    list_ = Reference(list_id, List.id)
-    parser =  Reference(parser_id, Parser.id)
-
-
-class Query(object):
-    
-    __storm_table__ = 'query'
-    
-    id = Int(primary=True)
-    source_id = Int()
-    update_time = Unicode()
-    type = Int()
-    checksum = Unicode()
-    creation_time = Unicode()
-
-    source =  Reference(source_id, Source.id)
-    
-    
-class QueryDomain(object):
-    
-    __storm_table__ = 'query_domain'
-   
-    id = Int(primary=True)
-    domain_id = Int() 
-    query_id = Int() 
-
-    query = Reference(query_id, Query.id)
-    domain = Reference(domain_id, Domain.id)
-
-
-class QueryPort(object):
-    
-    __storm_table__ = 'query_port'
-   
-    id = Int(primary=True)
-    port_id = Int() 
-    query_id = Int() 
-
-    query = Reference(query_id, Query.id)
-    port = Reference(port_id, Port.id)
-
-
-class QueryIP(object):
-    
-    __storm_table__ = 'query_ip'
-   
-    id = Int(primary=True)
-    ip_id = Int() 
-    query_id = Int() 
-
-    query = Reference(query_id, Query.id)
-    ip = Reference(ip_id, IP.id)
-
-
-class QueryIPPort(object):
-    
-    __storm_table__ = 'query_ip_port'
-   
-    id = Int(primary=True)
-    ip_port_id = Int() 
-    query_id = Int() 
-
-    query = Reference(query_id, Query.id)
-    ip_port = Reference(id, IPPort.id)
-
-
-class Filter(object):
-
-    __storm_table__ = 'filter'
-
-    id = Int(primary=True)
-    query_id = Int()
-    protocol_version = Unicode()
-    protocol = Unicode()
-    tos = Int()
-    packets = Int()
-    bytes = Int()
-
-    query = Reference(query_id, Query.id)
-
-
-class Subscription(object):
-    
-    __storm_table__ = 'subscription'
-    
-    id = Int(primary=True)
-    type = Int()
-    name = Unicode()
-    timeout = DateTime()
-
-
-class SubscriptionPackets(object):
-    
-    __storm_table__ = 'subscription_packets'
-    
-    id = Int(primary=True)
-    subscription_id = Int()
-    query_id = Int()
-    tags = Unicode()
-
-    subscription = Reference(subscription_id, Subscription.id)
-    query = Reference(query_id, Query.id)
-
-
 class Prefix(object):
 
     __storm_table__ = 'prefix'
@@ -193,4 +27,278 @@ class Plugin(object):
     prefix = Reference(prefix_id, Prefix.id) 
 
 
+class Parser(object):
+   
+    __storm_table__ = 'parser'
+
+    id = Int(primary=True)
+    name = Unicode()
+    time_interval = Int()
+
+
+class Threat(object):
+
+    __storm_table__ = 'threat'
+    
+    id  = Int(primary=True)
+    type = Unicode()
+
+
+class Query(object):
+    
+    __storm_table__ = 'query'
+    
+    id = Int(primary=True)
+    source_id = Int()
+    update_time = Unicode()
+    type = Int()
+    checksum = Unicode()
+    creation_time = Unicode()
+
+    source =  Reference(source_id, Source.id)
+
+
+class Source(object):
+
+    __storm_table__ = 'source'
+
+    id = Int(primary=True)
+    name = Unicode()
+    link = Unicode()
+    checksum = Unicode()
+    threat_id = Int()
+    parser_id = Int()
+
+    threat = Reference(threat_id, Threat.id)
+    parser =  Reference(parser_id, Parser.id)
+
+
+class Subscription(object):
+    
+    __storm_table__ = 'subscription'
+    
+    id = Int(primary=True)
+    type = Int()
+    name = Unicode()
+    timeout = DateTime()
+
+
+class SubscriptionPackets(object):
+    
+    __storm_table__ = 'subscription_packets'
+    
+    id = Int(primary=True)
+    subscription_id = Int()
+    query_id = Int()
+    tags = Unicode()
+
+    subscription = Reference(subscription_id, Subscription.id)
+    query = Reference(query_id, Query.id)
+
+
+class Statistics(object):
+
+    __storm_table__ = 'statistics'
+
+    id = Int(primary=True)
+    query_id = Int()
+
+    query = Reference(query_id, Query.id)
+
+
+class IP(object):
+
+    __storm_table__ = 'ip'
+
+    id = Int(primary=True)
+    ip = Unicode()
+    ip_int = Int()
+
+
+class Port(object):
+
+    __storm_table__ = 'port'
+
+    id = Int(primary=True)
+    port = Int()
+
+
+class SrcIP(object):
+
+    __storm_table__ = 'src_ip'
+
+    id = Int(primary=True)
+    ip_id = Int()
+    query_id = Int()
+
+    query = Reference(query_id, Query.id)
+    ip = Reference(ip_id, IP.id)
+
+ 
+class DstIP(object):
+
+    __storm_table__ = 'dst_ip'
+
+    id = Int(primary=True)
+    ip_id = Int()
+    query_id = Int()
+
+    query = Reference(query_id, Query.id)
+    ip = Reference(ip_id, IP.id)
+
+    
+class SrcPort(object):
+    
+    __storm_table__ = 'src_port'
+   
+    id = Int(primary=True)
+    port_id = Int() 
+    query_id = Int() 
+
+    query = Reference(query_id, Query.id)
+    port = Reference(port_id, Port.id)
+
+
+class DstPort(object):
+    
+    __storm_table__ = 'dst_port'
+   
+    id = Int(primary=True)
+    port_id = Int() 
+    query_id = Int() 
+
+    query = Reference(query_id, Query.id)
+    port = Reference(port_id, Port.id)
+
+
+class Proto(Object):
+    
+    __storm_table__ = 'proto'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    proto = Unicode()
+    
+    query = Reference(query_id, Query.id)
+
+
+class ProtocolVersion(Object):
+    
+    __storm_table__ = 'protocol_version'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    proto = Unicode()
+    
+    query = Reference(query_id, Query.id)
+
+
+class Packets(Object):
+    
+    __storm_table__ = 'packets'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    packets = Int()
+
+    query = Reference(query_id, Query.id)
+
+
+class Bytes(Object):
+    
+    __storm_table__ = 'bytes'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    bytes = Int()
+    
+    query = Reference(query_id, Query.id)
+
+
+
+class Duration(Object):
+    
+    __storm_table__ = 'duration'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    duration = Int()
+
+    query = Reference(query_id, Query.id)
+
+
+class Flags(Object):
+    
+    __storm_table__ = ''
+  
+    id = Int(primary=True)
+    query_id = Int()
+    flags = Unicode()
+    
+    query = Reference(query_id, Query.id)
+
+
+class Tos(Object):
+    
+    __storm_table__ = 'tos'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    tos = Int()
+    
+    query = Reference(query_id, Query.id)
+
+
+class PPS(Object):
+    
+    __storm_table__ = 'pps'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    pps = Int()
+    
+    query = Reference(query_id, Query.id)
+
+
+class BPS(Object):
+    
+    __storm_table__ = 'bps'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    bps = Int()
+    
+    query = Reference(query_id, Query.id)
+
+
+class BPP(Object):
+    
+    __storm_table__ = 'bpp'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    bpp = Int()
+    
+    query = Reference(query_id, Query.id)
+
+
+class ASN(Object):
+    
+    __storm_table__ = 'ASN'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    asn = Unicode()
+    
+    query = Reference(query_id, Query.id)
+
+class Scale(Object):
+    
+    __storm_table__ = 'scale'
+  
+    id = Int(primary=True)
+    query_id = Int()
+    scale = Unicode()
+
+    query = Reference(query_id, Query.id)
 
