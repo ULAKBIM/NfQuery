da441fedf6eea9dcceeb2dec1339ca8f29f75522
ackepenek <ahmetcan196@gmail.com>
2012-06-11 13:47:12 +0000

    added welcome.php and about.php files

---
+++ plugin/frontend/nfquery/welcome.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <div class="row-fluid">
+			<div class="span4">
+				<fieldset><strong>Recent Queries</strong>
+			</div>
+			</div>
+            	<div class="row-fluid">
+			 		<div class="span4">
+             			<table data-spy="scroll" data-offset="0" class="table table-striped table-bordered table-condensed">
+                		<?php
+                    	?>
+                		</table>
+
+			</div>
+			</div>
+            	<div class="row-fluid">
+					<div class="span4">
+				 	<fieldset><strong>Alerts</strong>
+				</div>
+			</div>
+            <div class="row-fluid">
+				<div class="span4">
+ 					<table>
+					 <?php
+                        ?>  
+                	</table>
+
+				</div>
+			</div>
+	</body>
+</html>
