2fc3d281934b05cc24203ebbdf77dc0e5fb97f40
srdemircan <demircan.serhat@gmail.com>
2012-06-11 14:03:12 +0000

    conflicts merged.

--- plugin/frontend/nfquery/subscriptions.php
+++ plugin/frontend/nfquery/subscriptions.php
@@ -1,15 +1,14 @@
 <?php 
-	require_once('/var/www/nfsen/conf.php');
 	require_once('/var/www/nfsen/lookup.php');
 	require_once('/var/www/nfsen/nfsenutil.php');
 ?>
-
-<div class="row-fluid">
-	<header>
-		<h3>Subscription Management</h3>
-	</header>
-	<div class="span4" id="subscription_list"  style="max-height:150px;">
-		<table data-spy="scroll" data-offset="0" class="table table-striped table-bordered table-condensed">
+<div class="row">
+<h3>Subscription Management</h3>
+</div>
+<div class="row-fluid" style="padding-top:10px">
+	<div class="span4" id="subscription_list">
+		<table class="table table-striped table-bordered table-condensed" style="cursor:pointer">
+			<tr><th>Subscriptions</th></tr>
 			<?php
 				// the command to be executed in the backend plugin
 				$command = 'nfquery::getSubscriptions';
@@ -23,8 +22,8 @@
 			?>
 		</table>
 	</div>
-	<div class="span7" id="subscription_details">
-		<table data-spy="scroll" data-offset="0" class="table table-striped table-bordered table-condensed">
+	<div class="span7" id="subscription_details" style="max-height:150px;overflow:auto;">
+		<table class="table table-striped table-bordered table-condensed">
 			<tr><th>Details Of Selected Subscription</th></tr>
 		</table>
 	</div>
