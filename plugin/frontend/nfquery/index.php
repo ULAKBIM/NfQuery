43add8bf5f1d0878afb0099b74ea21d56315a8b7
srdemircan <demircan.serhat@gmail.com>
2012-06-11 11:02:30 +0000

    nfquery/index.php added.

---
+++ plugin/frontend/nfquery/index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="/nfsen/css/bootstrap.css" />
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+		  	  <div class="span12">
+				<div class="tabbable tabs-left">
+					<ul class="nav nav-tabs">
+						<li class="active"><a data-toggle="tab" href="#">Home</a></li>
+						<li><a data-toggle="tab" href="#">Subscription</a></li>
+						<li><a data-toggle="tab" href="#">Report</a></li>
+						<li><a data-toggle="tab" href="#">Settings</a></li>
+						<li><a data-toggle="tab" href="#">About</a></li>
+					</ul>
+
+	    			<div class="tab-content">
+					</div>
+			
+				</div>
+			  </div>
+			</div>
+		</div>
+	<script src="http://code.jquery.com/jquery-1.7.2.js"></script>
+	<script src="/nfsen/js/bootstrap.js"></script>
+	<body>
+</html>
