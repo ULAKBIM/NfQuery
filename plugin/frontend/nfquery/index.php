18dfc1128360746268ae5ecf1cd67351110e754a
srdemircan <demircan.serhat@gmail.com>
2012-06-11 12:42:51 +0000

    Added toggle button to enable/disable subscriptions.

--- plugin/frontend/nfquery/index.php
+++ plugin/frontend/nfquery/index.php
@@ -3,19 +3,24 @@
 		<link rel="stylesheet" href="/nfsen/css/bootstrap.css" />
 	</head>
 	<body>
-		<div class="container">
-			<div class="row">
+		<div class="container-fluid">
+			<div class="row-fluid">
 		  	  <div class="span12">
 				<div class="tabbable tabs-left">
 					<ul class="nav nav-tabs">
-						<li class="active"><a data-toggle="tab" href="#">Home</a></li>
-						<li><a data-toggle="tab" href="#">Subscription</a></li>
+						<li><a data-toggle="tab" href="">Home</a></li>
+						<li><a data-toggle="tab" href="#subscriptions">Subscription</a></li>
 						<li><a data-toggle="tab" href="#">Report</a></li>
 						<li><a data-toggle="tab" href="#">Settings</a></li>
 						<li><a data-toggle="tab" href="#">About</a></li>
 					</ul>
 
-	    			<div class="tab-content">
+					<div class="tab-content">
+						<div class="tab-pane" id="subscriptions">
+							<div class="container-fluid">
+								<?php include('subscriptions.php'); ?>
+							</div>
+						</div>
 					</div>
 			
 				</div>
@@ -24,5 +29,6 @@
 		</div>
 	<script src="http://code.jquery.com/jquery-1.7.2.js"></script>
 	<script src="/nfsen/js/bootstrap.js"></script>
+	<script src="/nfsen/js/subscription.js"></script>
 	<body>
 </html>
