0f2dee4e25e9ab462f9942bf89b4d86754e96553
ackepenek <ahmetcan196@gmail.com>
2012-06-14 03:08:59 +0000

    added getSubscriptionDetail() function to nfsenutil.php

--- plugin/frontend/nfquery/nfqueryutil.php
+++ plugin/frontend/nfquery/nfqueryutil.php
@@ -6,8 +6,17 @@
 	function getSubscriptions(){
 		$command = 'nfquery::getSubscriptions';
 		$opts = array();
+		echo "asdff"
 		$out_list = nfsend_query($command, $opts);
 		$subscriptions = $out_list['subscriptions'];
+		return $subscriptions;
+	}
+	function getSubscriptionsDetail($name){
+		$command = 'nfquery::getSubscriptionsDetail';
+		$opts = array();
+		$opts['name'] = $name;
+		$out_list = nfsend_query($command, $opts);
+		$subscriptions = $out_list['subscriptiondetail'];
 		return $subscriptions;
 	}
 
