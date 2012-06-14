9dac65c84f94e9ef2de0bda1368a04f6b6bb3d48
srdemircan <demircan.serhat@gmail.com>
2012-06-14 08:41:00 +0000

    index.php tab navigation process changed.

--- plugin/frontend/nfquery/welcome.php
+++ plugin/frontend/nfquery/welcome.php
@@ -1,32 +1,28 @@
-<html>
-    <body>
-        <div class="row-fluid">
-			<div class="span4">
-				<fieldset><strong>Recent Queries</strong>
-			</div>
-			</div>
-            	<div class="row-fluid">
-			 		<div class="span4">
-             			<table data-spy="scroll" data-offset="0" class="table table-striped table-bordered table-condensed">
-                		<?php
-                    	?>
-                		</table>
+<div class="row-fluid">
+	<div class="span4">
+		<fieldset><strong>Recent Queries</strong>
+	</div>
+</div>
+<div class="row-fluid">
+ 		<div class="span4">
+   			<table data-spy="scroll" data-offset="0" class="table table-striped table-bordered table-condensed">
+          		<?php
+             	?>
+			</table>
 
-			</div>
-			</div>
-            	<div class="row-fluid">
-					<div class="span11">
-				 	<fieldset><strong>Alerts</strong>
-				</div>
-			</div>
-            <div class="row-fluid">
-				<div class="span11">
- 					<table>
-						<?php
-                        ?>  
-                	</table>
+		</div>
+</div>
+<div class="row-fluid">
+	<div class="span11">
+	 	<fieldset><strong>Alerts</strong>
+	</div>
+</div>
+<div class="row-fluid">
+	<div class="span11">
+		<table>
+			<?php
+            ?>  
+       	</table>
 
-				</div>
-			</div>
-	</body>
-</html>
+	</div>
+</div>
--- plugin/frontend/nfquery/workspace.php
+++ plugin/frontend/nfquery/workspace.php
@@ -1,12 +1,8 @@
-<html>
-    <body>
-        <div class="row-fluid">
-			<div class="span11">
-				<?php
-					DisplayDetails();
-					DisplayProcessing();
-
-                ?>  
-			</div>
-	</body>
-</html>
+<div class="row-fluid">
+	<div class="span11">
+		<?php
+			DisplayDetails();
+			DisplayProcessing();
+        ?>  
+	</div>
+</body>
