3e5370b6b986eadd2bb105ea4473c1e4484a1657
srdemircan <demircan.serhat@gmail.com>
2012-06-18 13:51:57 +0000

    function added to get subscripted subscriptions.

--- plugin/frontend/nfquery/workspace.php
+++ plugin/frontend/nfquery/workspace.php
@@ -2,7 +2,27 @@
 	<div class="span11">
 		<?php
 			DisplayDetails();
-			DisplayProcessing();
+			#DisplayProcessing();
         ?>  
+	</div>
+</body>
+
+<div class="row-fluid">
+	<div class="span4">
+		<h3>Run Queries</h3>
+		<?php
+			require_once('nfqueryutil.php');
+			
+			$subscripted = getSubscriptedSubscription();
+
+			echo '<label>'.'Select subscription: '.'</label>';		
+			echo '<select id="subscripted">';
+			foreach($subscripted as $s){
+				echo '<option>'.$s.'</option>';
+			}
+			echo '</select>';
+
+			echo '<a id="runQueries" class="btn btn-primary">Run !</a>'
+		?>  
 	</div>
 </body>
