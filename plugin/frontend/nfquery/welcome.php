0245fd0cbe3266fcfed7fd4a629c0870c32451b4
ackepenek <ahmetcan196@gmail.com>
2012-06-19 10:40:27 +0000

    change detail of subscription page

--- plugin/frontend/nfquery/welcome.php
+++ plugin/frontend/nfquery/welcome.php
@@ -19,17 +19,18 @@
 		 <?php
                         require_once('nfqueryutil.php');
 
-			$subscriptions = getSubscriptions();
+/*			$subscriptions = getMyAlerts();
 			for($i = 0; $i < sizeof($subscriptions); ++$i) {
                                 echo "<div class ='alert alert-block alert-error fade-in'>".$subscriptions[$i]."</div>\n";
 			
                         }
-/*
+
                        require_once('nfqueryutil.php');
 		       $myalerts = getMyAlerts();
 		       var_dump($myalerts);*/
 		?>
 		<script type="text/javascript" language="JavaScript">
+			getAlerts();
 		</script>
 		</div>
 	</div>
