6f037e532d6459c35fa8e0f34a5c8f034cdc6a5c
srdemircan <demircan.serhat@gmail.com>
2012-06-25 13:04:45 +0000

    * Mon Jun 25 16:01:31 EEST 2012     Navbar style changed.

--- plugin/frontend/nfquery/index.php
+++ plugin/frontend/nfquery/index.php
@@ -1,5 +1,6 @@
 		<link rel="stylesheet" href="/nfsen/css/bootstrap.css" />
 		<link rel="stylesheet" href="/nfsen/css/detail.css" />
+		<link rel="stylesheet" href="/nfsen/plugins/nfquery/css/query.css" />
 		<link rel="stylesheet" href="/nfsen/plugins/nfquery/css/process.css" /> <!-- Override bootstrap's form elements width and height-->
 		<script src="http://code.jquery.com/jquery-1.7.2.js"></script>
 		<script src="/nfsen/js/bootstrap.js"></script>
@@ -7,9 +8,9 @@
 		<script src="/nfsen/js/subscription.js"></script>
 		<div class="container-fluid">
 			<div class="row-fluid">
-		  	  <div class="span12">
-				<div class="tabbable tabs-left">
-					<ul class="nav nav-tabs">
+			  <div class="span2">
+					<h2>Nfquery</h2>
+					<ul class="nav nav-list">
 						<form method="post" action="/nfsen/nfsen.php" id="navigationForm"> 
 							<input type="hidden" name="nfqueryTabName" id="nfqueryTab"/>
 								<?php
@@ -23,16 +24,16 @@
 									}
 								?>
 						</form>
-							<li class="<?php if (strcmp($tabName, "Home") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav ">Home</a></li>
-							<li class="<?php if (strcmp($tabName, "Subscription") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">Subscription</a></li>
-							<li class="<?php if (strcmp($tabName, "Workspace") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">Workspace</a></li>
-							<li class="<?php if (strcmp($tabName, "Running") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">Running</a></li>
-							<li class="<?php if (strcmp($tabName, "Report") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">Report</a></li>
-							<li class="<?php if (strcmp($tabName, "Settings") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">Settings</a></li>
-							<li class="<?php if (strcmp($tabName, "About") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav">About</a></li>
+							<li class="<?php if (strcmp($tabName, "Home") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav "><i class="icon-home"></i>Home</a></li>
+							<li class="<?php if (strcmp($tabName, "Subscription") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-book"></i>Subscription</a></li>
+							<li class="<?php if (strcmp($tabName, "Workspace") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-pencil"></i>Workspace</a></li>
+							<li class="<?php if (strcmp($tabName, "Running") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-info-sign"></i>Running</a></li>
+							<li class="<?php if (strcmp($tabName, "Report") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-bullhorn"></i>Report</a></li>
+							<li class="<?php if (strcmp($tabName, "Settings") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-wrench"></i>Settings</a></li>
+							<li class="<?php if (strcmp($tabName, "About") == 0) echo "active"?>"><a data-toggle="tab" href="#" class="nfqueryNav"><i class="icon-align-justify"></i>About</a></li>
 					</ul>
-
-					<div class="tab-content">
+				</div>
+					<div class="tab-content span10">
 
 						<div class="tab-pane <?php if (strcmp($tabName, "Home") == 0) echo "active"?>" id="home">
 							<div class="container-fluid">
--- plugin/frontend/nfquery/workspace.php
+++ plugin/frontend/nfquery/workspace.php
@@ -10,7 +10,7 @@
 	<h3>Run Queries</h3>
 </div>
 <div class="row-fluid">
-	<div class="span5">
+	<div class="span6">
 		<?php
 			require_once('nfqueryutil.php');
 			
@@ -22,9 +22,9 @@
 				foreach($subscripted as $s){
 					echo '<div class="accordion-group">';
 					echo '<div class="accordion-heading">';
-					echo '<table><tr>';
+					echo '<table class="accordion-heading-table"><tr>';
 					echo '<td><input type=checkbox class="markAllMandatory" name="'.$s.'"/></td>';
-					echo '<td><a href="#'.$s.'" data-parent="#accordion" data-toggle="collapse" class="accordion-toggle"><i class="icon-plus"></i>'.$s.' </a></td>';
+					echo '<td><a href="#'.$s.'" data-parent="#accordion" data-toggle="collapse" class="accordion-toggle"><i class="icon-chevron-down"></i>'.$s.' </a></td>';
 					echo '</tr></table>';
 					echo '</div>';
 						echo '<div id="'.$s.'" style="padding-left:30px;" class="accordion-body collapse in subscriptionBody">';
@@ -36,11 +36,11 @@
 							  if (strcmp($query['category_name'], 'mandatory') == 0){
 								$has_query = true;
 								echo '<input type=checkbox class="mandatory_filter" name="'.$query['query_id'].'" >';
-								echo '<span class="label label-warning" style="font-size:12px;border-radius: 3px 3px 3px 3px;cursor:pointer" data-toggle="collapse" data-target="#optional'.$query['query_id'].'"><i class="icon-plus"></i>'.$query['filter'].'</span></input>';
+								echo '<span class="mandatory_query" style="border-radius: 3px 3px 3px 3px;cursor:pointer" data-toggle="collapse" data-target="#optional'.$query['query_id'].'"><i class="icon-chevron-down"></i>'.$query['filter'].'</span><span class="mandatory_query_popover badge badge-warning">M</span></input>';
 								echo '<div style="padding-left:30px;" id=optional'.$query['query_id'].' class="collapse in">';
 							  }elseif(strcmp($query['category_name'], 'optional') == 0){
 								echo '<input type=checkbox  class="optional_filter" name="'.$query['query_id'].'">';
-								echo '<span class="label label-success" style="font-size:12px;border-radius: 3px 3px 3px 3px;">'.$query['filter'].'</span></input>';
+								echo '<span class="optional_query" style="border-radius: 3px 3px 3px 3px;">'.$query['filter'].'</span><span class="optional_query_popover badge badge-success">O</span></input>';
 								echo '<br>';
 							  }
 							}
