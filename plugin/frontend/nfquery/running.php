19404acd92c790a08797caad2058f657d73ad4a4
srdemircan <demircan.serhat@gmail.com>
2012-06-29 13:04:36 +0000

    added lookup popover for srcip and dstip fields in output table.

--- plugin/frontend/nfquery/running.php
+++ plugin/frontend/nfquery/running.php
@@ -6,9 +6,12 @@
 	</div>
 </div>
 
+
 <div class="row-fluid">
 	<div class="span12">
 		<div  id="queryStatusDiv">
+			<div id="lookupBox" style="width:300px; height:300px; visibility: hidden;">
+			</div>
 			<?php
 				require_once('nfqueryutil.php');
 				
