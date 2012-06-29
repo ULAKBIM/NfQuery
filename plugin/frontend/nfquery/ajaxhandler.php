6cfeb6441faf58b4249deb19f4b301143823104d
ackepenek <ahmetcan196@gmail.com>
2012-06-29 12:50:58 +0000

    changed the config file template

--- plugin/frontend/nfquery/ajaxhandler.php
+++ plugin/frontend/nfquery/ajaxhandler.php
@@ -7,12 +7,19 @@
 		$map = $_POST['map'];
 		$json = json_encode($map);
 		$fp = fopen('/home/ahmetcan/nfquery/plugin/backend/nfquery.plugin.conf', 'w');
-		$file = fopen("/var/www/nfsen/plugins/nfquery/remember.conf","w");
+		$file = fopen("/var/www/nfsen/plugins/nfquery/remember.conf","a");
 		$res = array();
 		$res['prefix'] = $_POST['prefix'];
+		fwrite($fp,"#####################################\n");
+		fwrite($fp,"######  Nfquery Plugin Conf #########\n");
+		fwrite($fp,"#####################################\n\n\n");
+		foreach($map as $key => $value){
+			fwrite($fp,"#$key\n");
+			fwrite($fp,"$key =  $value\n\n");
+		}
 		fwrite($file,json_encode($res));
-		fwrite($fp, $json);
 		fclose($fp);
+		fclose($file);
 	}
 
 	if(isset($_POST['getAlerts'])){
