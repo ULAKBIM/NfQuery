a974008850ff3dda10c7e9e95efcdcf3b9b60ebc
srdemircan <demircan.serhat@gmail.com>
2012-07-03 13:43:23 +0000

    matched/total statistics added to running tab.

--- plugin/frontend/nfquery/running.php
+++ plugin/frontend/nfquery/running.php
@@ -2,6 +2,16 @@
 <script src="/nfsen/plugins/nfquery/js/jquery.tablesorter.js"></script>
 <link type="text/css" href="/nfsen/plugins/nfquery/css/tablesorter.css" rel="stylesheet">
 
+<div class="modal hide fade" id="outputModal" style="display: none;width:100%;">
+	<div class="modal-header">
+        <h3>Query Output</h3>
+        </div>
+        <div class="modal-body" id="outputModalBody">
+        </div>
+        <div class="modal-footer">
+          	<a data-dismiss="modal" class="btn" href="#">Close</a>
+       	</div>
+</div>
 <div class="row-fluid">
 	<div class="span4">
 		<strong>Running Queries</strong>	
