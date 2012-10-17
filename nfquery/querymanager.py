e87a15fbc1af213ab929de12eaf231e98607f5c3
Ahmet Can KEPENEK <ahmetcan196@gmail.com>
2012-10-17 16:33:16 +0300

    alert into database which added push alert method

--- nfquery/querymanager.py
+++ nfquery/querymanager.py
@@ -560,6 +560,15 @@ class QueryManager:
         return list(subscription_list)
 
 
+    def pushAlert(self, query_id_list, plugin_ip):
+        plugin_id = self.store.find( Plugin.id,Plugin.plugin_ip == unicode(plugin_ip)).one()
+        print plugin_id
+        for query_id in query_id_list:
+            alert = Alert()
+            alert.query_id = query_id
+            alert.plugin_id= plugin_id
+            self.store.add(alert)
+
     def registerAlert(self, alert):
         pp = pprint.PrettyPrinter(indent=4)
         print '\n'
