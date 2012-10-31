f7584ab7d5a40f2daa0efa439eed28a614a741ff
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2012-10-31 20:34:30 +0200

    Bootstrap is updated to version 2.2.1

--- plugin/frontend/nfquery/welcome.php
+++ plugin/frontend/nfquery/welcome.php
@@ -1,17 +1,23 @@
+<?php
+   require_once('nfqueryutil.php');
+
+   $alerts = getMyAlerts();
+   
+   $identifier_alerts = $alerts['identifier']; 
+   $identified_alerts = $alerts['identified']; 
+   
+   var_dump($identifier_alerts);
+?>
+
 <div class="row-fluid">
 	<div class="span11">
-	 	<fieldset><strong>Alerts</strong>
-		<div id="alertDiv">
-		 <?php
-            require_once('nfqueryutil.php');
+        <h2>Alerts</h2>
+	</div>
+</div>
 
-            $alerts = getMyAlerts();
-            
-            $identifier_alerts = $alerts['identifier']; 
-            $identified_alerts = $alerts['identified']; 
-            
-            var_dump($identifier_alerts);
-		?>
+<div class="row-fluid">
+	<div class="span11">
+		<div id="identifierAlertsDiv">
 		</div>
 	</div>
 </div>
