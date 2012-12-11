24a489498ab3d6e3430347d00b83625ec8c09fb9
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2012-12-11 16:33:12 +0200

    Informative text added fixes #20

--- plugin/backend/nfquery.pm
+++ plugin/backend/nfquery.pm
@@ -250,6 +250,7 @@ sub checkQueries{
 		push @{$args{'subscriptions'}}, $subscription;
         my %states = &checkPidStatesOfSubscription($subscription);
         @args{keys %states} = values %states;
+
 	}
 		
 
@@ -551,6 +552,18 @@ sub getStatisticsOfSubscription{
 
 	my $queries = $stats->{$subscriptionName};
 
+
+    my @temp = @{$running_subscriptions->{$subscriptionName}{'sources'}};
+    my @sources;
+
+    foreach my $source (@temp){
+        push @sources, $source;
+    }
+
+    $args{"sources"} = \@sources;
+    $args{"start_time"} = $running_subscriptions->{$subscriptionName}{'start_time'};
+    $args{"end_time"} = $running_subscriptions->{$subscriptionName}{'end_time'};
+
 	foreach my $query_id ( keys %$queries ){
 		my %fields = %{$queries->{$query_id}};
 
@@ -835,6 +848,7 @@ sub runQueries{
 
         syslog('debug', "INITIALIZING $subscription_name");
         $running_subscriptions->{$subscription_name} = {};
+        $running_subscriptions->{$subscription_name}{'sources'} = \@source;
         $running_subscriptions->{$subscription_name}{'start_time'} = $start_time;
         $running_subscriptions->{$subscription_name}{'end_time'} = $end_time;
         $running_subscriptions->{$subscription_name}{'mandatory'} = {};
