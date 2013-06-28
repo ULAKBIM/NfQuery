2d4217b362521daddfc7ba11edf6271152309623
Emre Yüce <amboss@mynet.com>
2013-06-28 10:33:56 +0300

    comments deleted, code cleaned

--- queryserver/setup.py
+++ queryserver/setup.py
@@ -13,41 +13,21 @@
 #
 # Copyright NfQuery Team Members
 
-#import sys
-#import os
-#from distutils.core import setup
-
 from setuptools import setup
 
 setup( 
        name='nfquery',
        version='1.0',
-       author = "Serdar Yigit",
+       author = "Serdar Yigit, Serhat Rifat Demircan, Ahmet Can Kepenek",
        author_email = "syigitisk@gmail.com",
        description = ("A thing"),
        license = "BSD",
        keywords = "test setup",
-       #url = "http://packages.python.org/an_example_pypi_project",
-	   packages=['nfquery'],
+       packages=['nfquery'],
        scripts=['bin/nfqueryd'],
-	   zip_safe = False,
-	   #install_requires = ['MySQLdb',],
+       zip_safe = False,
        data_files = [
-            ('/etc/',['/home/serhat/nfquery/cfg/nfquery.conf'])
-#      		('/etc/',['/home/ahmetcan/Workspace/projects/cfg/nfquery.conf'])
-#           ('/etc/',['/home/hamza/nfquery/cfg/nfquery.conf'])
+            ('/etc/',['./cfg/nfquery.conf'])
        ],
-	   #entry_points = {
-	   # 	'console_scripts': [
-	   # 		#'nfquery = nfquery.nfquery:go',
-       #         'nfqueryd = bin.nfqueryd'
-	   # 	]	
-	   #	},
      )
-
-#import os
-#os.system('mv /usr/local/bin/nfqueryd /usr/local/bin/Nfquery')
-
-#os.chown('/usr/bin/nfquery.py','serdar','serdar')
-#os.chown('/etc/nfquery.conf','serdar','serdar')
 
